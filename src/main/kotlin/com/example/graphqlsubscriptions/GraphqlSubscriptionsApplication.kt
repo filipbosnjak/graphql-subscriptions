41d80809d12566c4d172a94a6c0c9e60a7db222b
Filip.Bosnjak <filipo.bosnjak@gmail.com>
2023-08-28 09:47:37 +0200

    init commit

---
+++ src/main/kotlin/com/example/graphqlsubscriptions/GraphqlSubscriptionsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphqlsubscriptions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlSubscriptionsApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlSubscriptionsApplication>(*args)
+}
